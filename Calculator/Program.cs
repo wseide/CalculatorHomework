0ab3bc821c706e7a262e067043e6949c98a15294
Wismy <Wismy@HQ-WIM-LT>
2021-06-02 20:11:47 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 42;
+            int b = 119;
+
+        }
+    }
+}
